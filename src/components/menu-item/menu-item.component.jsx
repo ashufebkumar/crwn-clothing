34d728abab6244ae6cedf1e9263010032885518d
ashufeb_parkwheels <ashutosh.kumar1@parkwheels.co.in>
2020-12-17 13:44:31 +0530

    routing completed

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+import {withRouter} from 'react-router-dom'
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div 
+  	className={`${size} menu-item`} 
+  	onClick={() => history.push(`${match.url}${linkUrl}`)} 
+  >
     <div
       className='background-image'
       style={{
@@ -11,10 +16,13 @@ const MenuItem = ({ title, imageUrl, size }) => (
       }}
     />
     <div className='content'>
+
       <h1 className='title'>{title.toUpperCase()}</h1>
+
       <span className='subtitle'>SHOP NOW</span>
+      
     </div>
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
